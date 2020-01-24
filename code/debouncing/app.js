e47160e8be5452ea2fcb05987acce5197ad45e99
codecrook <hello@codecrook.io>
2020-01-24 22:37:26 +0530

    Added the debounce function

--- code/debouncing/app.js
+++ code/debouncing/app.js
@@ -1,18 +1,32 @@
 {
     'use strict';
+    
+    /**
+     * @param {Function} fn     An event-handeler function that you wanto add debouncing on
+     * @param {Number}   delay  Time in miliseconds after which you want the function to be called
+     * @returns {Function}      A debounced event handeler function
+     */
+    function debounce(fn, delay) {
+        let delayTimer;
+        
+        return function (e) {
+            clearTimeout(delayTimer);
+            delayTimer = setTimeout(fn.bind(null, e), delay)
+        };
+    }
+
     const addItemInput = document.querySelector('#add-item');
     const itemContainer = document.querySelector('#item-box');
-    
+
     const addItem = (e) => {
         if (!e.key.match(new RegExp('[0-9]'))) return;
         
         const newItem = document.createElement('div');
         newItem.classList.add('item');
-        newItem.innerHTML = e.key;
+        newItem.innerHTML = addItemInput.value;
 
         itemContainer.appendChild(newItem);
     };
 
-    addItemInput.addEventListener('keyup', addItem);
+    addItemInput.addEventListener('keyup', debounce(addItem, 1000));
 }
-
